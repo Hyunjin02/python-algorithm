9d9bb525b975c490202fc2d18d8e142d9611853b
Hyunjin02 <70645020+Hyunjin02@users.noreply.github.com>
2023-07-04 16:57:14 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -1,17 +1,11 @@
 a,b=map(int,input().split())
-c=[[0 for i in range(0,b)] for j in range(0,a)]
-d=[[0 for i in range(0,b)] for j in range(0,a)]
-e=[[0 for i in range(0,b)] for j in range(0,a)]
-
-for i in range(0,a):
-    c[i]=list(map(int,input().split()))
-for i in range(0,a):
-    d[i]=list(map(int,input().split()))
-for i in range(0,a):
-    for j in range(0,b):
-        e[i][j]=c[i][j]+d[i][j]
-
-for i in range(0,a):
-    for j in range(0,b):
-        print(e[i][j],end=' ')
+c=[]
+d=[]
+for i in range(a):
+    c.append(list(map(int,input().split())))
+for i in range(a):
+    d.append(list(map(int,input().split())))
+for i in range(a):
+    for j in range(b):
+        print(c[i][j]+d[i][j],end=" ")
     print()
