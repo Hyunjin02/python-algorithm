2c53ed805e0c32f08711204dd3e56c69b07453de
Hyunjin02 <70645020+Hyunjin02@users.noreply.github.com>
2023-08-29 15:58:52 +0900

    [Silver IV] Title: 스택 2, Time: 1208 ms, Memory: 71248 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+
+arr = []
+n=int(input().rstrip())
+for i in range(n):
+    order_input = input().split()
+    order = int(order_input[0])
+    if len(order_input) > 1:
+        x = int(order_input[1])
+
+    if(order==1):
+        arr.append(x)
+
+    elif(order==2):
+        if(len(arr)>0):
+            print(arr.pop())
+        else:
+            print(-1)
+
+    elif(order==3):
+        print(len(arr))
+
+    elif(order==4):
+        if(len(arr)==0):
+            print(1)
+        else:
+            print(0)
+
+    elif(order==5):
+        if(len(arr)==0):
+            print(-1)
+        else:
+            print(arr[len(arr)-1])
